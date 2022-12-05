f62949d5413f3ece5558d39cd886367fd5c9a71a
Danil <nnaumov538@gmail.com>
2022-12-05 11:22:39 +0500

    Add code

---
+++ GachiV2.py
@@ -0,0 +1,35 @@
+# Гачи Version 2
+
+from datetime import datetime
+
+while True:
+	password = input("\nGive your password \n\n>>> ")
+	if password == "world cum shot":
+		password = False
+		worldcs = input("\nwcs? \n\n>>> ")
+		if worldcs == "yes" or worldcs == "Yes" or worldcs == "YES":
+			vremya = datetime.now().hour
+			if 7 <= vremya < 23:
+				print("Now " + str(vremya) + " hours, mef day, aeeee)))0))0))")
+				exit()
+			elif print("\nSpim ZzZz..."):
+				exit()
+		else:
+			print("\nFUCK YOU LEATHER MAN!!!!")
+			exit()
+	else:
+		while (password) != False:
+			password = input("\nPassword is not correct. Try again \n\n>>> ")
+			if password == "world cum shot":
+				password = False
+				worldcs = input("\nwcs? \n\n>>> ")
+				if worldcs == "yes" or worldcs == "Yes" or worldcs == "YES":
+					vremya = datetime.now().hour
+					if 7 <= vremya < 23:
+						print("Now " + str(vremya) + " hours, mef day, aeeee)))0))0))")
+						exit()
+					elif print("\nSpim ZzZz..."):
+						exit()
+				else:
+					print("\nFUCK YOU LEATHER MAN!!!!")
+					exit()
